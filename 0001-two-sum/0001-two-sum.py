1638f9649b7ec7e2e59716616ca8252eadba78b5
Harshendra-45 <singh.04825@gmail.com>
2026-06-29 23:36:18 +0530

    Time: 0 ms (100%), Space: 13.1 MB (53.18%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        
+        hashmap = {}
+
+        for i in range(len(nums)):
+
+            if target - nums[i] in hashmap:
+                return [hashmap[target - nums[i]], i]
+
+            else:
+                hashmap[nums[i]] = i
