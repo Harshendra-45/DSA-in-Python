ee20800eee08e2c3b990edccb643195be75473b6
Harshendra-45 <singh.04825@gmail.com>
2026-06-29 23:40:01 +0530

    Time: 8 ms (94.99%), Space: 12.8 MB (63.44%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,9 @@
+class Solution(object):
+
+    def isPalindrome(self, s):
+        p="!@#$%^&*(){}[]\_-:;,`.?'/\""
+        s=s.replace(" ","")
+        s=s.lower()
+        for i in p:
+            s=s.replace(i,"")
+        return s==s[::-1]
