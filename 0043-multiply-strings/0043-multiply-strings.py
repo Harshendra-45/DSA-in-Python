a64b8eb92e404ed9d411d3add94ac4e3df339387
Harshendra-45 <singh.04825@gmail.com>
2026-07-08 09:31:47 +0530

    Time: 0 ms (100%), Space: 19.3 MB (66.28%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -1,8 +1,4 @@
-class Solution(object):
-    def multiply(self, num1, num2):
-        """
-        :type num1: str
-        :type num2: str
-        :rtype: str
-        """
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
         return str(int(num1)*int(num2))
+        
